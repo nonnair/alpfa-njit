c5715102c987acdccf17f21a1e6cdfbaf6e97462
Riannon <riannon817@gmail.com>
2025-11-21 15:38:03 -0500

    Missing text

--- src/components/Join.jsx
+++ src/components/Join.jsx
@@ -5,7 +5,7 @@ function Join() {
   return (
     <section id="join">
       <div className="join-container">
-        <h2 className="section-title">J</h2>
+        <h2 className="section-title">Join</h2>
         <div className="join-list">
         </div>
       </div>
